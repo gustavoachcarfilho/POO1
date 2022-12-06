5688b64f4fab32496b75217f39f9fbc857f80d01
Gustavo Achcar Filho <gustavofilho@greal.com.br>
2022-12-06 17:45:08 -0300

    Atualizacao do ex05

---
+++ LAB03 - Inheritance/ex05/LAB03ex05/src/Horista.java
@@ -0,0 +1,29 @@
+public class Horista extends Funcionarios{
+    private String nome;
+    private String dataNasc;
+    private String salario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+}
---
+++ LAB03 - Inheritance/ex05/LAB03ex05/src/Vendedor.java
@@ -0,0 +1,29 @@
+public class Vendedor extends Funcionarios{
+    private String nome;
+    private String dataNasc;
+    private String salario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+}
